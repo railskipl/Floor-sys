1d5c9f379b694a0ad4192a736e694973cf9da3a8
Amol <rails@kunalinfotech.net>
2012-04-18 17:25:22 +0530

    user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+      @users = User.all
+
+        respond_to do |format|
+          format.html # index.html.erb
+          format.xml  { render :xml => @users }
+        end
+    end
+    
   def dashboard
       @users = User.all
    end
