a0968261c30b5d9681dfbe47b53153d1ee5c73ac
Amol <rails@kunalinfotech.net>
2012-04-24 12:51:13 +0530

    amazon s3 credential update

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,9 +4,14 @@ class Company < ActiveRecord::Base
         :styles => { :thumb => "80x75" },
        
        
-        :storage => :s3, 
-        :bucket => "floor_sys", :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-                :path => "company/:id/:style/:basename.:extension",
+        
+                
+                :storage => :s3,
+                                :s3_credentials => Rails.root.join("config/s3.yml"),
+                                :bucket => 'floor_sys',
+                                :s3_permissions => :public_read,
+                                :path => "company/:id/:style/:basename.:extension",
+                
         
         :convert_options => {
               :thumb => "-background '#F7F4F4' -compose Copy -gravity center -extent 80x75"
