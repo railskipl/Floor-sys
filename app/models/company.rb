a2d2ecbc9c9e0b763eefcddce02589eadfcf9b1b
Amol <rails@kunalinfotech.net>
2012-04-25 12:42:48 +0530

    s3 file update

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,17 +1,15 @@
 class Company < ActiveRecord::Base
   
   has_attached_file :logo, 
+        :styles => { :thumb => "80x75" },
+        
+        :storage => :s3, 
+        :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
         :path => "company/:id/:style/:basename.:extension", 
-        :storage => :s3,
-        :bucket => ENV['floor_sys'],
-        :s3_permissions => 'authenticated-read',
-        :s3_protocol => "http",
-        :s3_credentials => { :access_key_id => ENV['AKIAIWWQF5REKTC4ZYGQ'],
-                             :secret_access_key => ENV['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'] },
-        :styles => { :thumb => "120x120>" }
+        
+        :convert_options => {
+              :thumb => "-background '#F7F4F4' -compose Copy -gravity center -extent 80x75"
+          }
           
-          
-                          
-          
-                  
+                           
 end
