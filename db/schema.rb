c18117d49257adc16450ef90227c60998de218b5
Amol <rails@kunalinfotech.net>
2012-04-25 12:08:29 +0530

    supplier module update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424105520) do
+ActiveRecord::Schema.define(:version => 20120425061010) do
 
   create_table "categories", :force => true do |t|
     t.string   "category_name"
@@ -204,6 +204,10 @@ ActiveRecord::Schema.define(:version => 20120424105520) do
     t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", :force => true do |t|
