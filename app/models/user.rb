8e4f166008b3362ea561c9ee9e55ebbb008150de
Amol <rails@kunalinfotech.net>
2012-05-03 11:20:37 +0530

    login page updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  attr_accessor :login
+  
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,:company_id, :first_name, :surname, :job_title, :address_line1, :address_line2, :address_line3, :city, :postcode, :fix_line, :mobile_number, :county, :username, :role_ids, :admin_id, :login, :userimage
+  attr_accessible :email, :password, :password_confirmation, :remember_me,:company_id, :first_name, :surname, :job_title, :address_line1, :address_line2, :address_line3, :city, :postcode, :fix_line, :mobile_number, :county, :username, :role_ids, :admin_id, :userimage
   mount_uploader :userimage, UserimageUploader
   
   validates_presence_of             :first_name, :surname, :job_title, :address_line1, :address_line2, :address_line3, 
@@ -25,68 +25,7 @@ class User < ActiveRecord::Base
   
                                             
          
-      def update_with_password(params={}) 
-      if params[:password].blank? 
-        params.delete(:password) 
-        params.delete(:password_confirmation) if 
-    params[:password_confirmation].blank? 
-      end 
-
-      update_attributes(params) 
-    end
      
-    
-
-    def self.find_for_authentication(warden_conditions)
-       conditions = warden_conditions.dup
-       login = conditions.delete(:login)
-       where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-     end
      
-     protected
-     
-     def self.reset_password_keys
-     [:login]
-     end
-
-      # Attempt to find a user by it's email. If a record is found, send new
-      # password instructions to it. If not user is found, returns a new user
-      # with an email not found error.
-      def self.send_reset_password_instructions(attributes={})
-        recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-        recoverable.send_reset_password_instructions if recoverable.persisted?
-        recoverable
-      end 
-
-      def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
-        (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
-
-        attributes = attributes.slice(*required_attributes)
-        attributes.delete_if { |key, value| value.blank? }
-
-        if attributes.size == required_attributes.size
-          if attributes.has_key?(:login)
-             login = attributes.delete(:login)
-             record = find_record(login)
-          else  
-            record = where(attributes).first
-          end  
-        end  
-
-        unless record
-          record = new
-
-          required_attributes.each do |key|
-            value = attributes[key]
-            record.send("#{key}=", value)
-            record.errors.add(key, value.present? ? error : :blank)
-          end  
-        end  
-        record
-      end
-
-      def self.find_record(login)
-        where(["username = :value OR email = :value", { :value => login }]).first
-      end
       
 end
