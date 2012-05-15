079f4cf2f3dd3ae1789dead59f66e84b58b60a7b
Your Name <rails@kunalinfotech.net>
2012-05-15 15:07:48 +0530

    design integration

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,6 @@ class Category < ActiveRecord::Base
   # has_ancestry line make sures that entered sub category has parent category or not. this is the ancestry gem functionality
   has_ancestry
   
-  has_many :sub_categories, :dependent => :destroy
-    accepts_nested_attributes_for :sub_categories, :reject_if => lambda { |a| a[:sub_category_name].blank? }, :allow_destroy => true
+  
     
 end
